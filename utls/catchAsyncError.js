0015329e9d86d5609ece9cf51f2775264ef6a759
MohamedBarbary <mohamedalbarbary0@gmail.com>
2023-11-03 23:29:05 +0200

    initial commit

--- utls/catchAsyncError.js
+++
@@ -1,8 +0,0 @@
-// better than catch error
-//tricky here is that is a two calls call for fun
-// then call for the route when need it
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
